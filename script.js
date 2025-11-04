841c6a6e16eba5342dce5c6ef350833abaa07d9c
defalt11-code <royilao1114@gmail.com>
2025-11-04 11:07:50 +0800

    add a gameBoard function to create a board

---
+++ script.js
@@ -0,0 +1,16 @@
+function gameBoard() {
+    const board = [];
+    const row = 3;
+    const column = 3;
+
+    for(let i = 0; i < row; i++) {
+        board[i] = []; 
+        for(let j = 0; j < column; j++) {
+            for
+        }
+    }
+
+    return board
+}
+
+console.log(gameBoard())
