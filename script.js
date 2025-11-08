1213dd6fed082c4790c421d84fff221a2c760157
defalt11-code <royilao1114@gmail.com>
2025-11-08 21:31:49 +0800

    Add the comments for clarity

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* 	Experience teaches you how things are done.
+		Passion dares to ask if they could be done 
+		better.  */
+
+
+//Seperate the logic of the creation of board and return only what's needed
 function gameBoard() {
 	const board = [];
 	const row = 3;
@@ -23,6 +29,7 @@ function gameBoard() {
 
 const board = gameBoard();
 
+//Handles the creation of the player and automatically assign the mark 
 const createPlayer = function () {
 	const playerOne = (name) => ({ name: name, mark: "x" })
 	const playerTwo = (name) => ({ name: name, mark: "o" })
@@ -32,22 +39,26 @@ const createPlayer = function () {
 const p1 = createPlayer.playerOne("john");
 const p2 = createPlayer.playerTwo("Josh");
 
+// This control the whole flow of the game
 const gameController = function () {
 	const { getBoard, setMove, resetBoard } = board;
 	let players = [];
 	let activePlayer;
 
+	//Set the players position once game starts
 	const setPlayers = (p1, p2) => {
 		players = [p1, p2];
 		activePlayer = players[0];
 	}
 
+	//Handles of switching of players everytime we play a round
 	const switchActive = () => {
 		activePlayer = activePlayer === players[0] ? players[1] : players[0];
 	};
 
 	const getActivePlayer = () => activePlayer;
 
+	//Check the board Status if all cells ar taken then reset the board 
 	const checkBoard = () => {
 		const boardStatus = getBoard().reduce((cells, rows) => cells.concat(rows), []);
 		if (boardStatus.every(cell => cell != "")) {
@@ -56,6 +67,7 @@ const gameController = function () {
 		}
 	}
 
+	//Handles each round this is where we call all helper function to check each round
 	const playRound = (r, c) => {
 		if (getBoard()[r][c] === "x" || getBoard()[r][c] === "o") {
 			alert("cell are already taken!");
@@ -69,6 +81,7 @@ const gameController = function () {
 		return getBoard();
 	}
 
+	//Check if theres already winner using the pattern that we map if its mactches the pattern
 	const checkWinner = () => {
 		const winningPatterns = [
 			[[0, 0], [0, 1], [0, 2]],
@@ -79,8 +92,9 @@ const gameController = function () {
 			[[0, 2], [1, 2], [2, 2]],
 			[[0, 0], [1, 1], [2, 2]],
 			[[0, 2], [1, 1], [2, 0]],
-		]
+		];
 
+		//map the patterns and store it the marks variable and check if marks has all the same mark
 		for (const patterns of winningPatterns) {
 			const marks = patterns.map((position) => {
 				const r = position[0];
