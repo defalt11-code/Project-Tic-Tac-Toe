150adf383ed8148c64690a77003355157350b01f
defalt11-code <royilao1114@gmail.com>
2025-11-08 20:45:11 +0800

    finish the checkWinner

--- script.js
+++ script.js
@@ -1,75 +1,97 @@
 function gameBoard() {
-    const board = [];
-    const row = 3;
-    const column = 3;
+	const board = [];
+	const row = 3;
+	const column = 3;
 
-    for(let i = 0; i < row; i++) {
-        board[i] = []; 
-        for(let j = 0; j < column; j++) {
-            board[i][j] = "";
-        }
-    }
+	for (let i = 0; i < row; i++) {
+		board[i] = [];
+		for (let j = 0; j < column; j++) {
+			board[i][j] = "";
+		}
+	}
 
-    const getBoard = () => board;
-    const setMove = (r, c, mark) => board[r][c] = mark;
+	const getBoard = () => board;
+	const setMove = (r, c, mark) => board[r][c] = mark;
 
-    return {getBoard, setMove};
+	return { getBoard, setMove };
 }
 
-const board = gameBoard()
+const board = gameBoard();
 
-const createPlayer = function() {
-  const playerOne = (name) => ({name: name, mark: "x"})
-  const playerTwo = (name) => ({name: name,  mark: "o"})
-  return {playerOne, playerTwo}
+const createPlayer = function () {
+	const playerOne = (name) => ({ name: name, mark: "x" })
+	const playerTwo = (name) => ({ name: name, mark: "o" })
+	return { playerOne, playerTwo }
 }()
 
 const p1 = createPlayer.playerOne("john");
 const p2 = createPlayer.playerTwo("Josh");
 
-const gameController = function() {
-    const { getBoard, setMove } = board;
+const gameController = function () {
+	const { getBoard, setMove } = board;
+	let players = [];
+	let activePlayer;
 
-    const players = [p1, p2];
-    let activePlayer = players[0];
-    const switchActive = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
+	const setPlayers = (p1, p2) => {
+		players = [p1, p2];
+		activePlayer = players[0];
+	}
 
-    const getActivePlayer = () => activePlayer;
+	const switchActive = () => {
+		activePlayer = activePlayer === players[0] ? players[1] : players[0];
+	};
 
-    const playRound = (r, c) => {
-        if(getBoard()[r][c] === "x" || getBoard()[r][c] === "o") {
-            alert("cell are already taken!");
-            return;
-        }
-        setMove(r, c, activePlayer.mark);
-        switchActive();
-        return getBoard();
-    }
+	const getActivePlayer = () => activePlayer;
 
-    const checkWinner = () => {
-        const winningPatterns = [ 
-            [[0,0],[0,1],[0,2]],
-            [[1,0],[1,1],[1,2]],
-            [[2,0],[2,1],[2,2]],
-            [[0,0],[1,0],[2,0]],
-            [[0,1],[1,1],[2,1]],
-            [[0,2],[1,2],[2,2]],
-            [[0,0],[1,1],[2,2]],
-            [[0,2],[1,1],[2,0]],
-        ]
+	const playRound = (r, c) => {
+		if (getBoard()[r][c] === "x" || getBoard()[r][c] === "o") {
+			alert("cell are already taken!");
+			return;
+		} else {
+			setMove(r, c, activePlayer.mark);
+		}
+		checkWinner();
+		switchActive();
+		return getBoard();
+	}
 
-        for(const patterns of winningPatterns) {
-        }
-    }
+	const checkWinner = () => {
+		const winningPatterns = [
+			[[0, 0], [0, 1], [0, 2]],
+			[[1, 0], [1, 1], [1, 2]],
+			[[2, 0], [2, 1], [2, 2]],
+			[[0, 0], [1, 0], [2, 0]],
+			[[0, 1], [1, 1], [2, 1]],
+			[[0, 2], [1, 2], [2, 2]],
+			[[0, 0], [1, 1], [2, 2]],
+			[[0, 2], [1, 1], [2, 0]],
+		]
 
-    return {playRound, getActivePlayer, checkWinner};
+		for (const patterns of winningPatterns) {
+			const marks = patterns.map((position) => {
+				const r = position[0];
+				const c = position[1];
+				return getBoard()[r][c];
+			});
+			if (marks.every(mark => mark != "" && mark === marks[0])) {
+				console.log(`${activePlayer.name} wins!`);
+			}
+		}
+	}
+
+	return { setPlayers, playRound, getActivePlayer, };
 }()
 
+gameController.setPlayers(p1, p2);
+
 console.log(gameController.getActivePlayer());
-console.log(gameController.playRound(1, 2));
+console.log(gameController.playRound(0, 0));
 console.log(gameController.getActivePlayer());
 console.log(gameController.playRound(1, 1));
+console.log(gameController.getActivePlayer());
+console.log(gameController.playRound(2, 0));
+console.log(gameController.getActivePlayer());
+console.log(gameController.playRound(2, 1));
+console.log(gameController.getActivePlayer());
+console.log(gameController.playRound(1, 0));
 
-console.log(gameController.checkWinner())
